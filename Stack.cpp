698583578e751029fb0e0bd34b8540aa0ed038bf
Andrey <arteezybezskilla@mail.ru>
2016-09-18 11:14:55 +0300

    update stack

--- Stack.cpp
+++ Stack.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
-using namespace std;
+#pragma once
 
+#include <iostream>
+
+using namespace std;
 
 template <typename T>
 class stack
@@ -17,10 +19,8 @@ private:
 	size_t count_;
 };
 
-
 template <typename T>
-stack<T>::stack() : array_(nullptr), array_size_(0), count_(0){}
-
+stack<T>::stack() : array_(nullptr), array_size_(0), count_(0){};
 
 template <typename T>
 stack<T>::~stack()
@@ -28,32 +28,33 @@ stack<T>::~stack()
 	delete[] array_;
 }
 
-
 template <typename T>
 size_t stack<T>::count() const
-{
-	return count_;
+{ 
+	return count_; 
 }
-
 
 template <typename T>
 void stack<T>::push(T const & x)
 {
-	if (count_ == 0)
+	if (array_ == nullptr)
 	{
 		array_ = new T[1];
-		array_[count_] = x;
-		count_++; 
-		array_size_ = 1;
+		array_[0] = x;
+		count_++;
+		array_size_++;
 	}
 	else
 	{
-		T *array_2 = array_;
-		array_size_ = count_ + 1;
-		array_ = new T[array_size_];
-		for (int i = 0; i < count_; i++) array_[i] = array_2[i];
-		delete[] array_2;
-		array_[count_] = x;
+		if (count_ == array_size_)
+		{
+			T *p = array_;
+			array_ = new T[array_size_ * 2];
+			copy(p, p + count_, array_);
+			delete[]p;
+			array_size_ *= 2;
+		}
+		array_[count_] = a;
 		count_++;
 	}
 }
@@ -61,8 +62,7 @@ void stack<T>::push(T const & x)
 template <typename T>
 T stack<T>::pop()
 {
-	if (count_ == 0) throw "\n empty stack";
-	else
+	if (count_> 0) 
 	{
 		count_--;
 		return array_[count_];
@@ -70,11 +70,3 @@ T stack<T>::pop()
 }
 
 
-int main()
-{
-	stack<int> a;
-	a.push(2);
-	a.pop();
-	system("pause");
-	return 0;
-}
