d0ec69113b4c252b2e5fce40c359601b93f4d2f8
Arteezy09 <arteezybezskilla@mail.ru>
2016-09-18 18:10:51 +0400

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -8,11 +8,7 @@ SCENARIO("push", "[push]")
 {
   stack<int> st;
   st.push(1);
-  REQUIRE(st.count()==1);
   REQUIRE(st.pop()==1);
-  st.push(2);
-  REQUIRE(st.count()==2);
-
 }
 
 SCENARIO("count", "[count]")
