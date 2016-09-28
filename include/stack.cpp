82c42915007295b4663abbaceb6012d6f894f3fb
Arteezy09 <arteezybezskilla@mail.ru>
2016-09-28 15:08:26 +0300

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -12,6 +12,7 @@ public:
 	~stack();                           /* noexcept */
 
 	size_t count() const;               /* noexcept */
+	bool empty() const;                 /* noexcept */
 
 	stack & operator=(const stack &);   /* strong */
 	void push(T const &);               /* strong */
@@ -33,6 +34,13 @@ template <typename T>
 stack<T>::~stack()
 {
 	delete[] array_;
+}
+
+
+template<typename T>
+bool stack<T>::empty() const
+{ 
+	return(count_ == 0); 
 }
 
 
