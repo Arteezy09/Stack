c0327e73b1e6ed7f93b0f56c3b1672c6a0743560
Arteezy09 <arteezybezskilla@mail.ru>
2016-11-07 00:57:03 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -1,44 +1,45 @@
 #include <stack.cpp>
 #include <catch.hpp>
 #include <iostream>
-
 using namespace std;
 
-SCENARIO("count", "[count]")
-{
-  stack<int> st;
-  st.push(1);
-  REQUIRE(st.count()==1);
+SCENARIO("count", "[count]"){
+  stack<int> s;
+  s.push(1);
+  REQUIRE(s.count()==1);
 }
 
-SCENARIO("push_top", "[push_top]")
-{
-  stack<int> st;
-  st.push(1);
-  REQUIRE(st.top()==1);
+SCENARIO("push", "[push]"){
+  stack<int> s;
+  s.push(1);
+  REQUIRE(s.count()==1);
+  REQUIRE(s.top()==1);
 }
 
-SCENARIO("pop", "[pop]")
-{
-  stack<int> st;
-  st.push(1); 
-  st.pop();
-  REQUIRE(st.count()==0);
+SCENARIO("pop", "[pop]"){
+  stack<int> s;
+  s.push(1); s.pop();
+  REQUIRE(s.count()==0);
 }
 
-SCENARIO("cop", "[cop]")
-{
-  stack<int> st1;
-  st1.push(1);
-  stack<int> st2=st1;
-  REQUIRE(st2.count()==1);
-  REQUIRE(st2.top()==1);
+SCENARIO("prisv", "[prisv]"){
+  stack<int> s;
+  s.push(1);
+  stack<int> s2;
+  s2=s;
+  REQUIRE(s.count()==1);
+  REQUIRE(s.top()==1);
 }
 
-SCENARIO("empty", "[empty]")
-{
-  stack<int> st1, st2;
-  st1.push(1);
-  REQUIRE(!st1.empty());
-  REQUIRE(st2.empty());
+SCENARIO("top", "[top]"){
+  stack<int> s;
+  s.push(1);
+  REQUIRE(s.top()==1);
+}
+
+SCENARIO("empty", "[empty]"){
+  stack<int> s1, s2;
+  s1.push(1);
+  REQUIRE(!s1.empty());
+  REQUIRE(s2.empty());
 }
