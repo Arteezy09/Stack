fcd084fef62a104a8c46b826991de7e46dcc8ae5
Arteezy09 <arteezybezskilla@mail.ru>
2016-11-14 00:11:38 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -129,115 +129,96 @@ private:
 
 
 template <typename T>
-allocator<T>::allocator(size_t size) 
-	: ptr_(static_cast<T *>(size == 0 ? nullptr : operator new(size * sizeof(T)))), 
-        size_(size),
-        map_(std::make_unique<bitset>(size)) {
+allocator<T>::allocator(size_t size): ptr_(static_cast<T *>(size == 0 ? nullptr : operator new(size * sizeof(T)))), size_(size), map_(std::make_unique<bitset>(size)){
+
+};
+
+template<typename T>
+allocator<T>::allocator(allocator const & tmp) :allocator<T>(tmp.size_){
+	for (size_t i = 0; i < size_; ++i) {
+		construct(ptr_ + i, tmp.ptr_[i]);
+	}
 }
 
-
-template<typename T> 
+template <typename T>
 allocator<T>::~allocator() {
-	if (this->count() > 0) {
-        allocator<T>::destroy(ptr_, ptr_ + size_); 
+	if (map_->counter() > 0) {
+		destroy(ptr_, ptr_ + map_->counter());
 	}
-	operator delete(ptr_);	
-}
-
+	operator delete(ptr_);
+};
 
 template <typename T>
-auto allocator<T>::construct(T *ptr,T const & val) -> void
-{
-	if (ptr >= ptr_ && ptr < ptr_ + size_ && map_->test(ptr - ptr_)){
-		new(ptr)T(val);
-		map_->set(ptr - ptr_);
-	}
-	else { throw("Error!"); }
-}
-
-
-template <typename T>
-auto allocator<T>::destroy(T *ptr) -> void
-{
-	if (map_->test(ptr-ptr_))
-	throw std::logic_error("Error!");
-	ptr->~T();
-	map_->reset(ptr - ptr_);
-		
-}
-
-
-template<typename T>
-auto allocator<T>::resize() -> void
-{
-	allocator<T> all(size_ * 2 + (size_ == 0));
-	for (size_t i = 0; i < size_; ++i) 
-		all.construct(all.get() + i, ptr_[i]);
-	this->swap(all);
-}
-
-
-template<typename T>
-auto allocator<T>::get() -> T* 
-{ 
-	return ptr_; 
-}
-
-
-template<typename T>
-auto allocator<T>::get() const -> T const * 
-{ 
-	return ptr_; 
-}
-
-
-template<typename T>
-allocator<T>::allocator(allocator const & other) : allocator<T>(other.size_)
-{
-	for (size_t i = 0; i < size_; ++i) 
-		construct(ptr_ + i, other.ptr_[i]);
-}
-
-
-template <typename T>
-auto allocator<T>::destroy(T *first,T *last) -> void
-{
-	if(first>last)
-	throw std::logic_error("Error!");
-	for (; first != last; ++first)
-	{
-		destroy(&*first);
-	}
-	
-}
-
-
-template<typename T>
-auto allocator<T>::count() const -> size_t
-{ 
-	return map_->counter(); 
-}
-
-
-template<typename T>
-auto allocator<T>::full() const -> bool 
-{
-	return (map_->counter() == size_); 
-}
-
-
-template<typename T>
-auto allocator<T>::empty() const -> bool 
-{ 
-	return (map_->counter() == 0); 
-}
-
-	
-template<typename T> 
-auto allocator<T>::swap(allocator & other) -> void {
+auto allocator<T>::swap(allocator & other)->void {
 	std::swap(ptr_, other.ptr_);
 	std::swap(size_, other.size_);
 	std::swap(map_, other.map_);
+};
+
+template <typename T>
+auto allocator<T>::construct(T * ptr, T const & value)->void {
+	if (ptr < ptr_ || ptr >= ptr_ + size_) {
+		throw std::out_of_range("Error");
+	}
+	new(ptr) T(value);
+	map_->set(ptr - ptr_);
+	
+	
+	
+}
+
+template <typename T>
+auto allocator<T>::destroy(T * ptr)->void
+{
+
+	ptr->~T();
+	map_->reset(ptr - ptr_);	
+}
+
+
+template <typename T>
+auto allocator<T>::destroy(T * first, T * last)->void
+{
+	for (; first != last; ++first) {
+		destroy(&*first);
+	}
+}
+
+template<typename T>
+auto allocator<T>::resize()-> void {
+	size_t size = size_ * 2 + (size_ == 0);
+	allocator<T> buff(size);
+	for (size_t i = 0; i < size_; ++i) {
+	if (map_->test(i))
+{		buff.construct(buff.ptr_ + i, ptr_[i]);}
+	}
+	this->swap(buff);
+	size_ = size;
+}
+
+template<typename T>
+auto allocator<T>::empty() const -> bool {
+	return (map_->counter() == 0);
+}
+
+template<typename T>
+auto allocator<T>::full() const -> bool {
+	return (map_->counter() == size_);
+}
+
+template<typename T>
+auto allocator<T>::get() -> T * {
+	return ptr_;
+}
+
+template<typename T>
+auto allocator<T>::get() const -> T const * {
+	return ptr_;
+}
+
+template<typename T>
+auto allocator<T>::count() const -> size_t {
+	return map_->counter();
 }
 
 
@@ -266,9 +247,12 @@ private:
 
 	//auto throw_is_empty() const -> void;
 };
-//__________________________________________________________________________________________________________________
-//__________________________________________________________________________________________________________________
 
+
+/*template<typename T>
+auto stack<T>::throw_is_empty() const -> void {
+	throw std::logic_error("Error!"); 
+}*/
 
 template<typename T>
 auto stack<T>::empty() const->bool {
@@ -280,7 +264,6 @@ template <typename T>
 stack<T>::stack(size_t size) : allocate(size) {};
 
 
-
 template <typename T>
 auto stack<T>::push(T const &val)->void {
 	if (allocate.full()) {
@@ -288,7 +271,6 @@ auto stack<T>::push(T const &val)->void {
 	}
 	allocate.construct(allocate.get() + allocate.count(), val);
 }
-
 
 
 template <typename T>
@@ -305,11 +287,13 @@ auto stack<T>::count() const->size_t {
 	return allocate.count();
 }
 
+
 template <typename T>
 auto stack<T>::pop()->void {
 	if (allocate.count() == 0) throw std::logic_error("Empty!");
 	allocate.destroy(allocate.get() + (this->count()-1));
 }
+
 
 template <typename T>
 auto stack<T>::top() const->const T&{
@@ -317,6 +301,7 @@ auto stack<T>::top() const->const T&{
 return(*(allocate.get() + this->count() - 1));
 
 }
+
 
 template <typename T>
 auto stack<T>::top()->T&{
